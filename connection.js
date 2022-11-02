d49287e44e093162c7e4111db962b631a56c3540
Ray Davey <57007488+raymondfdavey@users.noreply.github.com>
2022-11-02 10:34:21 +0000

    cganges for heroku deployment

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
-// require("dotenv").config();
+require("dotenv").config();
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.DB_CONNECTION_STRING;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const connection = client.connect();
 
