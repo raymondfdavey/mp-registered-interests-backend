ac55149c0388cd70de3c9d6b21b8ae932f3579e3
Ray Davey <57007488+raymondfdavey@users.noreply.github.com>
2022-11-02 10:26:09 +0000

    cganges for heroku deployment

--- connection.js
+++ connection.js
@@ -6,4 +6,5 @@ const uri = process.env.DB_CONNECTION_STRING;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = client.connect();
+
 module.exports = client;
