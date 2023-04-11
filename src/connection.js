75afa54a59bc3a6864b23b35916948bebcd5d09b
Ray Davey <57007488+raymondfdavey@users.noreply.github.com>
2023-04-11 11:56:49 +0100

    stopped printing the url

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.DB_CONNECTION_STRING;
 
-console.log("PRINTING THE GOD DAMN FUCKING URI", uri);
+// console.log("PRINTING THE GOD DAMN FUCKING URI", uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
